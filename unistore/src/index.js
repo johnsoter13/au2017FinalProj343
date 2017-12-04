591998ad9fed1197493c4cbb3bdcd7c7f503df6c
John Soter <john.soter@outlook.com>
2017-12-04 13:07:42 -0800

    added firebase init

--- unistore/src/index.js
+++ unistore/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase/app'
+var config = {
+    apiKey: "AIzaSyBJN06qGCWV9dp3HemaMsRHWCCJPavcPbw",
+    authDomain: "unibookstore-f0cd0.firebaseapp.com",
+    databaseURL: "https://unibookstore-f0cd0.firebaseio.com",
+    projectId: "unibookstore-f0cd0",
+    storageBucket: "unibookstore-f0cd0.appspot.com",
+    messagingSenderId: "7215886852"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
