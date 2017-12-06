fa159a4140b556e7032689f72bb863794ef36bcb
Minsu Gim <minsugim@uw.edu>
2017-12-06 11:29:09 -0800

    Fixed a route and some CSS

--- unistore/src/Components/NavBar.js
+++ unistore/src/Components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import UniLogo from '../img/Wlogo.jpg';
 
     class NavBar extends Component {
@@ -7,7 +8,7 @@ import UniLogo from '../img/Wlogo.jpg';
           <nav id="navbar" className="navbar navbar-expand-lg navbar-dark bg-dark">
           <img id="unilogo" class="rounded-circle img-fluid" src={UniLogo} aria="University Logo"/>
           <a className="navbar-brand" href="/">
-            <h1>Uni<span>Store</span></h1>
+            <Link to="/" id="logo"><h1>Uni<span>Store</span></h1></Link>
             <h4>University of Washington</h4>
           </a>
           <button aria-controls="navbarToggler01" aria-expanded="false" aria-label="Toggle navigation" className="navbar-toggler" data-target="#navbarToggler01" data-toggle="collapse" type="button"><span className="navbar-toggler-icon"></span></button>
